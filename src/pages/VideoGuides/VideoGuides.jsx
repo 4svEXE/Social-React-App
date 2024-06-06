341f17d40b627ee072b026cd69f7df3ddfd56365
@4sv.exe <4sv.exe@gmail.com>
2024-06-06 12:49:35 +0300

    update(button) change styles

--- src/pages/VideoGuides/VideoGuides.jsx
+++ src/pages/VideoGuides/VideoGuides.jsx
@@ -1,40 +1,78 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import styles from "./VideoGuides.module.scss";
-// import PrimotyButton from "widgets/primotyButton/primotyButton";
-// import masd from "public/img/home/MASD.png";
+
+import ControllButton from "widgets/controllButton";
+
+// icons
 import play from "public/img/icons/play.svg";
 import pause from "public/img/icons/pause.svg";
+
+// videos
+// ! step 1
 import v1 from "public/video/v1.mp4";
-import ControllButton from "widgets/controllButton";
+import v2 from "public/video/v2.mp4";
+import v3 from "public/video/v3.mp4";
 
 export default function VideoGuides() {
   const { t } = useTranslation();
-  const [percentage, setPercentage] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null);
+  const [videoSrc, setVideoSrc] = useState("");
 
-  const handlePlayPause = () => {
-    const video = videoRef.current;
-    if (isPlaying) {
+  // ! Steps to add a new video-conteoller button:
+  // ! 1. Add a new video in the videoSrc state
+  // ! 2. update states percentages, isPlaying, videoRefs
+  // ! 3. add a new button in the handlePlayPause function
+  // ! 4. add a new video tag
+
+  // ! step 2
+  const [percentages, setPercentages] = useState({ v1: 0, v2: 0, v3: 0 });
+  const [isPlaying, setIsPlaying] = useState({
+    v1: false,
+    v2: false,
+    v3: false,
+  });
+  const videoRefs = {
+    v1: useRef(null),
+    v2: useRef(null),
+    v3: useRef(null),
+  };
+
+  const handlePlayPause = (videoKey) => {
+    const video = videoRefs[videoKey].current;
+    const currentIsPlaying = isPlaying[videoKey];
+    if (currentIsPlaying) {
       video.pause();
     } else {
       video.play();
     }
-    setIsPlaying(!isPlaying);
+    setIsPlaying({ ...isPlaying, [videoKey]: !currentIsPlaying });
   };
 
-  const handleTimeUpdate = () => {
-    const video = videoRef.current;
+  const handleTimeUpdate = (videoKey) => {
+    const video = videoRefs[videoKey].current;
     const percentage = (video.currentTime / video.duration) * 100;
-    setPercentage(percentage);
+    setPercentages({ ...percentages, [videoKey]: percentage });
+  };
+
+  const handleButtonClick = (src, videoKey) => {
+    if (videoSrc !== src) {
+      setVideoSrc(src);
+    }
+    handlePlayPause(videoKey);
   };
 
   useEffect(() => {
-    const video = videoRef.current;
-    video.addEventListener('timeupdate', handleTimeUpdate);
+    for (const videoKey in videoRefs) {
+      const video = videoRefs[videoKey].current;
+      video.addEventListener("timeupdate", () => handleTimeUpdate(videoKey));
+    }
     return () => {
-      video.removeEventListener('timeupdate', handleTimeUpdate);
+      for (const videoKey in videoRefs) {
+        const video = videoRefs[videoKey].current;
+        video.removeEventListener("timeupdate", () =>
+          handleTimeUpdate(videoKey)
+        );
+      }
     };
   }, []);
 
@@ -43,13 +81,57 @@ export default function VideoGuides() {
       <div className="w-full grid grid-cols-12 min-h-[50vh]">
         <div className="col-span-6">
           <div className={styles.controllButton}>
-            <ControllButton onClick={handlePlayPause} percentage={Math.floor(percentage)}>
-              <img src={isPlaying ? pause : play} alt="control" className={isPlaying ? styles.pause : styles.play} />
+            <ControllButton
+              onClick={() => {
+                handleButtonClick(v1, "v1");
+              }}
+              percentage={Math.floor(percentages["v1"])}
+            >
+              <img
+                src={isPlaying["v1"] ? pause : play}
+                alt="control"
+                className={isPlaying["v1"] ? styles.pause : styles.play}
+              />
+            </ControllButton>
+          </div>
+
+          <div className={styles.controllButton}>
+            <ControllButton
+              onClick={() => {
+                handleButtonClick(v2, "v2");
+              }}
+              percentage={Math.floor(percentages["v2"])}
+            >
+              <img
+                src={isPlaying["v2"] ? pause : play}
+                alt="control"
+                className={isPlaying["v2"] ? styles.pause : styles.play}
+              />
+            </ControllButton>
+          </div>
+
+          {/* // ! step 3 */}
+          <div className={styles.controllButton}>
+            <ControllButton
+              onClick={() => {
+                handleButtonClick(v3, "v3");
+              }}
+              percentage={Math.floor(percentages["v3"])}
+            >
+              <img
+                src={isPlaying["v3"] ? pause : play}
+                alt="control"
+                className={isPlaying["v3"] ? styles.pause : styles.play}
+              />
             </ControllButton>
           </div>
         </div>
+
         <div className="col-span-4">
-          <video ref={videoRef} src={v1} controls></video>
+          {/* // ! step 4 */}
+          <video ref={videoRefs['v1']} src={v1} controls style={{ display: videoSrc === v1 ? 'block' : 'none' }}></video>
+          <video ref={videoRefs['v2']} src={v2} controls style={{ display: videoSrc === v2 ? 'block' : 'none' }}></video>
+          <video ref={videoRefs['v3']} src={v3} controls style={{ display: videoSrc === v3 ? 'block' : 'none' }}></video>
         </div>
       </div>
     </div>
--- src/widgets/controllButton/index.jsx
+++ src/widgets/controllButton/index.jsx
@@ -7,14 +7,13 @@ const ControllButton = ({ onClick, percentage, children }) => {
 
   useEffect(() => {
     const duration = 5000;
-    const endValue = 251.2;
 
     const animateStroke = (timestamp, start = null) => {
       if (!start) start = timestamp;
       const progress = Math.min((timestamp - start) / duration, 1);
       const value = percentage;
       if (progressRef.current) {
-        progressRef.current.style.strokeDasharray = `${value / 100 * 115}, 251.2`;
+        progressRef.current.style.strokeDasharray = `${value / 100 * 132}, 251.2`;
       }
 
       if (progress < 1) {
@@ -27,21 +26,18 @@ const ControllButton = ({ onClick, percentage, children }) => {
 
   return (
     <div className="controll-button" onClick={onClick}>
-      <svg id="animated" width="40" height="40">
+      <svg id="animated" width="44" height="44">
         <circle
           ref={progressRef}
           id="progress"
-          cx="20"
-          cy="20"
-          r="18"
+          cx="22"
+          cy="22"
+          r="21"
           stroke="blue"
           strokeWidth="2"
           fill="none"
         />
       </svg>
-      {/* <div className="percentage">
-        {percentage}%
-      </div> */}
       <div className="control">
         {children}
       </div>
